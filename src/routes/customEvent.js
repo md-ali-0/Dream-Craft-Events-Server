257e3d76168a8638d9a46c6bb0dca432076f1a43
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2024-02-25 02:16:06 +0600

    updating custom event status

--- src/routes/customEvent.js
+++ src/routes/customEvent.js
@@ -5,5 +5,6 @@ import findCustomEvent from "../api/v1/customEvent/findCustomEvent.js"
 const router = Router()
 router.post('/custom-event', CustomEvents)
 router.get('/custom-event', findCustomEvent)
+router.patch()
 
 export default router;
