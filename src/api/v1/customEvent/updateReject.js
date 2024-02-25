3fc95bdb67d47c62aac7cb3549823debc6e217e0
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2024-02-25 17:00:50 +0600

    update status done for custom event

---
+++ src/api/v1/customEvent/updateReject.js
@@ -0,0 +1,24 @@
+import CustomEvent from "../../../models/CustomEvent.js";
+
+const rejectCustomEvent = async (req, res) => {
+    const filter = {
+        _id: req.params.id,
+    };
+    const update = {
+        $set: {
+            status: 'rejected'
+        }
+    }
+    try {
+
+        const updateStatus = await CustomEvent.findOneAndUpdate(filter, update)
+        console.log(updateStatus);
+        
+    } catch (error) {
+        console.error("Error updating status:", error)
+    }
+
+
+}
+
+export default rejectCustomEvent;
