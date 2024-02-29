65bb1d88c521d1f4426200b350c43ab261976840
aamamun24 <bdmamun100@gmail.com>
2024-02-29 15:29:37 +0600

    request organizer accept and reject api created

--- src/routes/organizerRequest/index.js
+++ src/routes/organizerRequest/index.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import findAll from "../../api/v1/organizerRequest/controllers/findAll.js";
 import postRequest from "../../api/v1/organizerRequest/controllers/postRequest.js";
+import requestAccept from "../../api/v1/organizerRequest/controllers/requestAccept.js";
+import requestReject from "../../api/v1/organizerRequest/controllers/requestReject.js";
 
 const router = Router()
 router.get('/request-organizer', findAll)
 router.post('/request-organizer', postRequest)
+router.put('/request-organizer/accept/:id', requestAccept)
+router.put('/request-organizer/reject/:id', requestReject)
 
 export default router
