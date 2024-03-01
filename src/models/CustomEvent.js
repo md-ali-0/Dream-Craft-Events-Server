e6d8d8ce2a666b527f2fbc732f2038f152833cf2
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2024-03-01 20:54:33 +0600

    edited custom event

--- src/models/CustomEvent.js
+++ src/models/CustomEvent.js
@@ -5,7 +5,6 @@ const CustomEventSchema = new Schema(
         'firstName' : {type: String, required: true},
         'lastName' : {type: String, required: true},
         'fullName' : {type: String, required: true},
-        'userEmail': {type: String, required: true},
         'email': {type: String, required: true},
         'phone': {type: String, required: true},
         'eventType': {type: String, required: true},
