32877761584f56d1e4f07fb47ab8cee83ee9674d
md-ali-0 <mohammad.9482@gmail.com>
2024-03-07 01:53:45 +0600

    user model changed

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,11 +2,20 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
     {
-        'name' : {type: String, required: true},
+        'fristname' : {type: String, required: true},
+        'lastname' : {type: String, required: true},
+        'phone' : {type: String},
         'email' : {type: String, required: true, unique: true},
         'image' : {type: String, required: true,
-                    default: 'https://placehold.co/500x500/png'
-                },
+            default: 'https://i.ibb.co/0XQDTZ1/user.png'
+        },
+        'cover' : {type: String, required: true,
+            default: 'https://i.ibb.co/SBJcwSg/cover.png'
+        },
+        'address' : {type: String},
+        'city' : {type: String},
+        'country' : {type: String},
+        'bio' : {type: String},
         'role': {type: String, required: true, enum: ['admin', 'organizer', 'user'], default: 'user'},
         'password' : { type: String, required: true},
         'createdAt': { type: Date, default: new Date()},
