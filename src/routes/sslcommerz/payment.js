87c54a5038881c51c99594604ce04853728d90a3
aamamun24 <bdmamun100@gmail.com>
2024-03-07 11:12:43 +0600

    payment fail api created

--- src/routes/sslcommerz/payment.js
+++ src/routes/sslcommerz/payment.js
@@ -3,10 +3,12 @@ import payment from "../../api/v1/sslcommerz/getPayment.js";
 import transId from "../../api/v1/sslcommerz/transId.js";
 import allPaymentHistory from "../../api/v1/sslcommerz/allPaymentHistory.js";
 import findAll from "../../api/v1/sslcommerz/findAll.js";
+import paymentFail from "../../api/v1/sslcommerz/paymentFail.js";
 
 const router = Router()
 router.post('/order', payment)
 router.post('/payment/success/:tranId', transId)
+router.post('/payment/fail/:tranId', paymentFail)
 router.get('/order/:email', allPaymentHistory)
 router.get('/bookings', findAll)
 
